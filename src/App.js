7620d2dc26a64213a775af64d144b28f3744d8b4
DCGrost <alamjguillen@gmail.com>
2023-11-30 12:29:45 -0600

    test3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Nuevo cambio
+          Edit <code>src/App.js</code> Nuevo cambio test
         </p>
         <a
           className="App-link"
